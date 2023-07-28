cb25c851127f1ccdfa5f5a6418a9f10d87c35c50
frederickraul <frederickraul@gmail.com>
2023-07-28 16:48:07 -0700

    Fix references Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,60 +1,58 @@
 'use client';
 
-import { IconType } from "react-icons/lib";
+import { IconType } from "react-icons";
 
-interface ButtonProps{
+interface ButtonProps {
   label: string;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   outline?: boolean;
-  small?:boolean;
-  icon?:IconType;
+  small?: boolean;
+  icon?: IconType;
 }
 
-const Button: React.FC<ButtonProps>  = ({
-  label,
-  onClick,
-  disabled,
+const Button: React.FC<ButtonProps> = ({ 
+  label, 
+  onClick, 
+  disabled, 
   outline,
   small,
-  icon: Icon
+  icon: Icon,
 }) => {
-  return (
-    <div
-      onClick={onClick}
+  return ( 
+    <button
       disabled={disabled}
+      onClick={onClick}
       className={`
         relative
-        disable:opacity-70
-        disable:cursor-not-allowed
+        disabled:opacity-70
+        disabled:cursor-not-allowed
         rounded-lg
         hover:opacity-80
         transition
         w-full
-        text-center
-        cursor-pointer
-        ${outline? 'bg-white': 'bg-green-500'}
-        ${outline? 'border-black': 'border-green-500'}
-        ${outline? 'text-black': 'text-white'}
-        ${small? 'py-1': 'py-3'}
-        ${small? 'text-sm': 'text-md'}
-        ${small? 'font-light': 'font-bold'}
-        ${small? 'border-[1px]': 'border-2'}
+        ${outline ? 'bg-white' : 'bg-rose-500'}
+        ${outline ? 'border-black' : 'border-rose-500'}
+        ${outline ? 'text-black' : 'text-white'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border-[1px]' : 'border-2'}
       `}
     >
-      { Icon && (
+      {Icon && (
         <Icon
           size={24}
           className="
             absolute
-             left-4
-             top-3
+            left-4
+            top-3
           "
         />
       )}
       {label}
-      </div>
-  )
+    </button>
+   );
 }
-
-export default Button
+ 
+export default Button;
