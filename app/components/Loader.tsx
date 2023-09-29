e5746290a4ffc4d305ec5a0e42d849218591051a
Zak Fonseca <frederickraul@hotmail.com>
2023-09-28 19:38:00 -0700

    - Fix Loader position

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => {
   return ( 
     <div className="z-10 w-[100%] h-[100%] bg-white opacity-70  absolute">
     <div
-      className="fixed left-[40%] sm:left-[45%]  md:left-[48%] top-[35%] z-10 "
+      className="fixed left-[37%] sm:left-[45%]  md:left-[48%] top-[35%] z-10 "
       >
       <PuffLoader
         size={100}
