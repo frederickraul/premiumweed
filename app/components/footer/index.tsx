0e0c4d02d0add069ea188cdc8e2951f31a753a08
Zak Fonseca <frederickraul@hotmail.com>
2023-10-21 11:19:41 -0700

    - Add footer logo link to main page

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -14,7 +14,9 @@ const Footer: React.FC<FooterProps> = ({currentUser}) => {
         bg-black absolute bottom-0 w-[100%]
         ">
                 <div className="text-gray-100 container mx-auto px-6 pt-10 pb-6 max-w-[960px] flex flex-col items-center" >
+                <a onClick={(e)=>e.preventDefault()} href="/">
                   <img src='/images/logo-white-text.png' width={250}/>
+                </a>
                   <span className='mt-2'>© Weedgrowers. All rights reserved.</span>
         </div>
             </div>
