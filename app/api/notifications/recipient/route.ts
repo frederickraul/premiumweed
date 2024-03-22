39331a7edbb00070d127c842aea3d59b51e661c6
Zak Fonseca <frederickraul@hotmail.com>
2024-03-21 18:55:30 -0700

    Fix Notification System Test 3

--- app/api/notifications/recipient/route.ts
+++ app/api/notifications/recipient/route.ts
@@ -4,60 +4,61 @@ import prisma from '@/app/libs/prismadb';
 import { NextResponse } from 'next/server'
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
+interface IParams {
+     lastNotificationId?: string;
+   }
 
 
-export async function GET() {
 
-     return NextResponse.json(null);
+   export async function GET(
+     request:Request,
+     { params }: { params: IParams }
+     ) {
+       const currentUser = await getCurrentUser();
+       if(!currentUser){
+         return NextResponse.error();
+       }
+       const { lastNotificationId } = params;
+
+    const notifications = await prisma.notification.findMany({
+      where:{
+        AND:[
+          {
+            recipientId: currentUser.id,
+          },{
+            status: 0,
+          }
+        ]
+      },
+      orderBy: [
+        {
+          status: 'asc',
+        },
+        {
+          timestamp: 'desc'
+        }
+      ],
+      take: 1,
+      include:{
+        sender:true,
+      }
+  });
+
+  const lastNotification = notifications[0];
+
+  if(!lastNotification){
+    const data = {
+      id:0,
+      timestamp: 0
+    }
+    return NextResponse.json(JSON.stringify(data));
+   }
+
+
+   //console.log(lastNotification);
+   
+   return NextResponse.json(lastNotification);
 
 }
-
-
-
-//     const currentUser = await getCurrentUser();
-//     if(!currentUser){
-//       return NextResponse.error();
-//     }
-    
-//     const notifications = await prisma.notification.findMany({
-//       where:{
-//         AND:[
-//           {
-//             recipientId: currentUser.id,
-//           },{
-//             status: 0,
-//           }
-//         ]
-//       },
-//       orderBy: [
-//         {
-//           status: 'asc',
-//         },
-//         {
-//           timestamp: 'desc'
-//         }
-//       ],
-//       take: 1,
-//       include:{
-//         sender:true,
-//       }
-//   });
-
-//   const lastNotification = notifications[0];
-
-//   if(!lastNotification){
-//     const data = {
-//       id:0,
-//       timestamp: 0
-//     }
-//     return NextResponse.json(JSON.stringify(data));
-//    }
-
-
-//    //console.log(lastNotification);
-   
-//    return NextResponse.json(lastNotification);
-
-// }
 
 
