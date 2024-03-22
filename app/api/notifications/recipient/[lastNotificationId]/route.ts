836e6b4f95b8ab84c7519e16f2e1dbef0ddbe83e
Zak Fonseca <frederickraul@hotmail.com>
2024-03-21 18:48:20 -0700

    Fix Notification System Test 2

--- app/api/notifications/recipient/[lastNotificationId]/route.ts
+++ app/api/notifications/recipient/[lastNotificationId]/route.ts
@@ -12,39 +12,34 @@ export async function POST(
   request:Request,
   { params }: { params: IParams }
   ) {
-
-        return NextResponse.json(null);
+    const currentUser = await getCurrentUser();
+    if(!currentUser){
+      return NextResponse.error();
+    }
+    const { lastNotificationId } = params;
+    
+    const notifications = await prisma.notification.findMany({
+      where:{
+        AND:[
+          {
+            recipientId: currentUser.id,
+          },{
+            status: 0,
+          }
+        ]
+      },
+      orderBy: [
+        {
+          status: 'asc',
+        },
+        {
+          createdAt: 'desc'
+        }
+      ],
+      include:{
+        sender:true,
+      }
+  });
+    
+    return NextResponse.json(notifications);
 }
-
-
-//     const currentUser = await getCurrentUser();
-//     if(!currentUser){
-//       return NextResponse.error();
-//     }
-//     const { lastNotificationId } = params;
-    
-//     const notifications = await prisma.notification.findMany({
-//       where:{
-//         AND:[
-//           {
-//             recipientId: currentUser.id,
-//           },{
-//             status: 0,
-//           }
-//         ]
-//       },
-//       orderBy: [
-//         {
-//           status: 'asc',
-//         },
-//         {
-//           createdAt: 'desc'
-//         }
-//       ],
-//       include:{
-//         sender:true,
-//       }
-//   });
-    
-//     return NextResponse.json(notifications);
-// }
